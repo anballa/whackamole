152a34daf64dd928ae7f8c06f881c125bd2c82a0
anballa <ananya.balla@gmail.com>
2024-11-12 14:03:37 -0500

    Update whackamole.py

--- whackamole.py
+++ whackamole.py
@@ -1,10 +1,6 @@
 import pygame
 import random
 
-SCREEN_WIDTH = 640
-SCREEN_HEIGHT = 512
-GRID_SIZE = 32
-
 def main():
     try:
         pygame.init()
@@ -25,28 +21,29 @@ def main():
                     mouse_x, mouse_y = event.pos
 
                 mole_rect = mole_image.get_rect(topleft=(mole_x, mole_y))
+                
                 if mole_rect.collidepoint(mouse_x, mouse_y):
-                    mole_x = random.randrange(0, SCREEN_WIDTH // GRID_SIZE) * GRID_SIZE
-                    mole_y = random.randrange(0, SCREEN_HEIGHT // GRID_SIZE) * GRID_SIZE
+                    mole_x = random.randrange(0, 640 // 32) * 32
+                    mole_y = random.randrange(0, 512 // 32) * 32
             
                                               
             screen.fill("light green")
             
             #horizontal lines
             for x in range(1, 20):
-                pygame.draw.line(screen, "black", (0,x*32), (640,x*32), 2)
+                pygame.draw.line(screen, "black", (0, x * 32), (640, x * 32), 2)
             
             #vertical lines
             for y in range(1, 40):
-                pygame.draw.line(screen, "black", (y*32, 0), (y*32, 512), 2)
+                pygame.draw.line(screen, "black", (y * 32, 0), (y * 32, 512), 2)
             
             screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
 
             pygame.display.flip()
             clock.tick(60)
+    
     finally:
         pygame.quit()
-
 
 if __name__ == "__main__":
     main()
