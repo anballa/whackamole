a6cf9f723e2febdecd3b00cdd296c40783fbefef
anballa <ananya.balla@gmail.com>
2024-11-12 14:54:32 -0500

    Update whackamole.py

--- whackamole.py
+++ whackamole.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+
 def main():
     try:
         pygame.init()
@@ -8,9 +9,9 @@ def main():
         clock = pygame.time.Clock()
 
         mole_image = pygame.image.load("mole.png")
-        mole_x = 0 
+        mole_x = 0
         mole_y = 0
-        
+
         running = True
         while running:
             for event in pygame.event.get():
@@ -18,32 +19,32 @@ def main():
                     running = False
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    mouse_x, mouse_y = event.pos
+                    x, y = event.pos
 
-                mole_rect = mole_image.get_rect(topleft=(mole_x, mole_y))
-                
-                if mole_rect.collidepoint(mouse_x, mouse_y):
-                    mole_x = random.randrange(0, 640 // 32) * 32
-                    mole_y = random.randrange(0, 512 // 32) * 32
-            
-                                              
+                    mole_rect = mole_image.get_rect(topleft=(mole_x, mole_y))
+
+                    if mole_rect.collidepoint(x, y):
+                        mole_x = random.randrange(0, 640 // 32) * 32
+                        mole_y = random.randrange(0, 512 // 32) * 32
+
             screen.fill("light green")
-            
-            #horizontal lines
+
+            # horizontal lines
             for x in range(1, 20):
                 pygame.draw.line(screen, "black", (0, x * 32), (640, x * 32), 2)
-            
-            #vertical lines
+
+            # vertical lines
             for y in range(1, 40):
                 pygame.draw.line(screen, "black", (y * 32, 0), (y * 32, 512), 2)
-            
-            screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
+
+            screen.blit(mole_image, (mole_x, mole_y))
 
             pygame.display.flip()
             clock.tick(60)
-    
+
     finally:
         pygame.quit()
+
 
 if __name__ == "__main__":
     main()
