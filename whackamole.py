1da22b36ad6333fe3ad1e73f479f31f110a7bacf
anballa <ananya.balla@gmail.com>
2024-11-12 12:54:43 -0500

    Update whackamole.py

--- whackamole.py
+++ whackamole.py
@@ -5,7 +5,7 @@ def main():
     try:
         pygame.init()
         # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
+        screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
@@ -17,6 +17,15 @@ def main():
             screen.fill("light green")
             pygame.display.flip()
             clock.tick(60)
+            #horizontal lines
+            for x in range(1, 20):
+                pygame.draw.line(screen, "black", (0,x*32), (640,x*32), 2)
+            #vertical lines
+            for y in range(1, 40):
+                pygame.draw.line(screen, "black", (y*32, 0), (y*32, 512), 2)
+
+            pygame.display.flip()
+            clock.tick(1)
     finally:
         pygame.quit()
 
